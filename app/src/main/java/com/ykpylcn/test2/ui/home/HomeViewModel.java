bd80165a642200ab24a34b6de1620e62eeca6ca5
Yakup YALCIN <yalcin.web@gmail.com>
2020-06-01 02:18:42 +0200

    Commit 1. deneme 2

--- app/src/main/java/com/ykpylcn/test2/ui/home/HomeViewModel.java
+++ app/src/main/java/com/ykpylcn/test2/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment test 2");
+        mText.setValue("This is home fragment test 3");
     }
 
     public LiveData<String> getText() {
